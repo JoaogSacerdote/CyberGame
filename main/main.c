185cda61257fd41429e157e31922560b9f7ed22d
joaogsacerdote <joaog.souza2002@gmail.com>
2026-03-25 14:02:11 -0300

    Finalizando config wokwi

--- main/main.c
+++ main/main.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
 
-void app_main() {
-  printf("Hello, Wokwi!\n");
-  while (true) {
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-  }
+void app_main(void) {
+    printf("Sistema iniciado! Wokwi rodando.\n");
 }
