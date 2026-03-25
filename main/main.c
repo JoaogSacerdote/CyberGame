71c0538a30c7a0fcda29025bd53fd9b0d4f61fc1
joaogsacerdote <joaog.souza2002@gmail.com>
2026-03-25 15:37:19 -0300

    att

--- main/main.c
+++ main/main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "Buton.h"
 
 void app_main(void) {
-    printf("Sistema iniciado! Wokwi rodando.\n");
+    printf("Sistema Iniciado! Configurando D-Pad...\n");
+
+    // Inicializa todos os botões
+    botoes_init();
+
+    printf("Botões prontos! Pressione-os no Wokwi.\n");
+
+    while (1) {
+        // Verifica cada botão individualmente
+        if (botao_pressionado(BTN_UP)) {
+            printf("-> CIMA (Pino 15) pressionado!\n");
+        }
+        if (botao_pressionado(BTN_DOWN)) {
+            printf("-> BAIXO (Pino 16) pressionado!\n");
+        }
+        if (botao_pressionado(BTN_LEFT)) {
+            printf("-> ESQUERDA (Pino 17) pressionado!\n");
+        }
+        if (botao_pressionado(BTN_RIGHT)) {
+            printf("-> DIREITA (Pino 18) pressionado!\n");
+        }
+        
+        // Pausa curta para leitura rápida e sem travar o chip
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
 }
