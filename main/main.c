6f766c65aa7246cda4e9c37dba48239cb237de9c
joaogsacerdote <joaog.souza2002@gmail.com>
2026-03-25 15:38:32 -0300

    Att

--- main/main.c
+++ main/main.c
@@ -1,32 +1,12 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "Buton.h"
+#include "esp_system.h"
+#include "esp_driver_gpio.h"
+#include "driver/gpio.h"
+// #include "Buton.h" 
+// #include "joystick.h"
 
 void app_main(void) {
-    printf("Sistema Iniciado! Configurando D-Pad...\n");
-
-    // Inicializa todos os botões
-    botoes_init();
-
-    printf("Botões prontos! Pressione-os no Wokwi.\n");
-
-    while (1) {
-        // Verifica cada botão individualmente
-        if (botao_pressionado(BTN_UP)) {
-            printf("-> CIMA (Pino 15) pressionado!\n");
-        }
-        if (botao_pressionado(BTN_DOWN)) {
-            printf("-> BAIXO (Pino 16) pressionado!\n");
-        }
-        if (botao_pressionado(BTN_LEFT)) {
-            printf("-> ESQUERDA (Pino 17) pressionado!\n");
-        }
-        if (botao_pressionado(BTN_RIGHT)) {
-            printf("-> DIREITA (Pino 18) pressionado!\n");
-        }
-        
-        // Pausa curta para leitura rápida e sem travar o chip
-        vTaskDelay(pdMS_TO_TICKS(100));
-    }
+   
 }
